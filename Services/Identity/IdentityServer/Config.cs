69fb1fd9cc61159c08ff04570515dd5b1b9afe0f
Эркин Камилов <erkimkamilov@gmail.com>
2020-10-13 00:34:34 +0500

    created identity server

--- Services/Identity/IdentityServer/Config.cs
+++ Services/Identity/IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,42 @@ namespace IdentityServer
         public static IEnumerable<ApiScope> ApiScopes =>
         new List<ApiScope>
         {
-                    new ApiScope("api1", "My API")
+            new ApiScope("mri", "MRI API")
         };
+        public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+                    // machine to machine client
+                    new Client
+                    {
+                        ClientId = "client",
+                        ClientSecrets = { new Secret("secret".Sha256()) },
+
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        // scopes that client has access to
+                        AllowedScopes = { "mri" }   // was api1
+                    },
+                
+                    // interactive ASP.NET Core MVC client
+                    new Client
+                    {
+                        ClientId = "mvc",
+                        ClientSecrets = { new Secret("secret".Sha256()) },
+
+                        AllowedGrantTypes = GrantTypes.Code,
+                    
+                        // where to redirect to after login
+                        //RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                        // where to redirect to after logout
+                        //PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                        AllowedScopes = new List<string>
+                        {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile
+                        }
+                    }
+    };
     }
 }
